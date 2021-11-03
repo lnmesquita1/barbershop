2c4ed0d364c2c059cec876e75d13bbf2b8631085
Lucas Nogueira Mesquita <amcom_lucas@badesc.gov.br>
2021-11-03 00:46:52 -0300

    Scrollable tab menu and chart

---
+++ src/components/Tabs.js
@@ -0,0 +1,98 @@
+import React, { useContext } from 'react';
+import { Icon } from 'react-native-elements';
+import styled from 'styled-components/native';
+import { useNavigation } from '@react-navigation/core';
+import { AuthContext } from '../config/AuthProvider';
+
+const iconColor = '#fff';
+
+export const Container = styled.View`
+  height: 170px;
+  margin-top: auto;
+`;
+
+export const TabsContainer = styled.ScrollView.attrs({
+  horizontal: true,
+  contentContainerStyle: { paddingLeft: 10, paddingRight: 20 },
+  showHorizontalScrollIndicator: false,
+})``;
+
+export const TabItem = styled.TouchableOpacity`
+  width: 100px;
+  height: 100px;
+  background: #323232;
+  border-radius: 3px;
+  margin-left: 10px;
+  margin-top: 40px;
+  padding: 10px;
+  align-items: flex-start;
+  justify-content: space-between;
+  border: 1px solid #323232;
+  box-shadow: -4px 4px 18px rgba(0, 0, 0, 0.2);
+`;
+
+export const TabText = styled.Text`
+  font-size: 13px;
+  color: #fff;
+`;
+
+export default props => {
+  const navigation = useNavigation();
+  const { logout } = useContext(AuthContext);
+
+    return (
+      <Container>
+        <TabsContainer>
+          <TabItem>
+            <Icon
+              name='calendar'
+              type='font-awesome'
+              color={iconColor} />
+            <TabText>Agenda</TabText>
+          </TabItem>
+          <TabItem onPress={() => navigation.navigate('ServicosListAdmin')}>
+            <Icon
+              name='cut'
+              type='font-awesome'
+              color={iconColor} />
+            <TabText>Serviços</TabText>
+          </TabItem>
+          <TabItem>
+            <Icon
+              name='male'
+              type='font-awesome'
+              color={iconColor} />
+            <TabText>Profissionais</TabText>
+          </TabItem>
+          <TabItem>
+            <Icon
+              name='address-card'
+              type='font-awesome'
+              color={iconColor} />
+            <TabText>Clientes</TabText>
+          </TabItem>
+          <TabItem>
+            <Icon
+              name='shopping-cart'
+              type='font-awesome'
+              color={iconColor} />
+            <TabText>Produtos</TabText>
+          </TabItem>
+          <TabItem>
+            <Icon
+              name='money'
+              type='font-awesome'
+              color={iconColor} />
+            <TabText>Financeiro</TabText>
+          </TabItem>
+          <TabItem onPress={ () => logout()}>
+            <Icon
+              name='sign-out'
+              type='font-awesome'
+              color={iconColor} />
+            <TabText>Sair</TabText>
+          </TabItem>
+        </TabsContainer>
+      </Container>
+    );
+}
--- src/screens/HomeAdmin/index.js
+++ src/screens/HomeAdmin/index.js
@@ -1,46 +1,113 @@
-import React, { useContext } from 'react';
-import { Button, Image, StatusBar, StyleSheet } from 'react-native';
-import { AuthContext } from '../../config/AuthProvider';
-import Text from '../../components/Text';
+import React from 'react';
+import styled from 'styled-components/native';
+import { Container } from './styles';
+import Tabs from '../../components/Tabs';
+import { Icon } from 'react-native-elements';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Roboto_500Medium } from '@expo-google-fonts/roboto';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
+import { Dimensions } from 'react-native';
 
-import { 
-    Container, 
-    CustomButton, 
-    CustomButtonText,
-    ServicesArea
-} from './styles';
-import { useNavigation } from '@react-navigation/core';
+const iconColor = 'black';
+
+export const ContainerDashboard = styled.View`
+  display: flex;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+
+export const HeaderArea = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const WelcomeMessage = styled.Text`
+  display: flex;
+  line-height: 20px;
+  margin-right: auto;
+  text-transform: uppercase;
+`;
+
+export const Chart = styled.View`
+  background: #FFFFFF;
+  margin-top: 20px;
+  height: 240px;
+  width: 100%;
+  border-radius: 20px;
+  box-shadow: 0px 0px 10px #EEF0F3;
+`;
 
 export default () => {
+  let [fontsLoaded] = useFonts({
+    Roboto_500Medium,
+  });
 
-    const { user, logout } = useContext(AuthContext);
-    const navigation = useNavigation();
-
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
     return (
-        <Container>
-            <StatusBar
-                animated={true}
-                barStyle='dark-content'/>
-            <Image style={styles.image} source={require('../../assets/logo.png')} />
-            <Text message={'Área do administrador'}/>
-            <ServicesArea>
-                <CustomButton onPress={() => navigation.navigate('ServicosListAdmin') }>
-                    <CustomButtonText>Serviços</CustomButtonText>
-                </CustomButton>
-                <CustomButton>
-                    <CustomButtonText>Quem somos</CustomButtonText>
-                </CustomButton>
-            </ServicesArea>
-            {/* <Text>{user?.email}</Text> */}
-            <Button title="logout" onPress={ () => logout()}></Button>
-        </Container>
+      <Container>
+        <ContainerDashboard>
+          <HeaderArea>
+            <WelcomeMessage style={{ fontFamily: 'Roboto_500Medium', fontSize: 17 }}>Bem vindo, Lucas</WelcomeMessage>
+            <Icon name='cog' type='font-awesome' color={iconColor} />
+          </HeaderArea>
+          <Chart>
+          <LineChart
+            data={{
+              labels: ["Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+              datasets: [
+                {
+                  data: [
+                    8,
+                    10,
+                    15,
+                    9,
+                    12,
+                    19
+                  ]
+                }
+              ]
+            }}
+            width={Dimensions.get("window").width - 60} // from react-native
+            height={220}
+            yAxisInterval={1} // optional, defaults to 1
+            chartConfig={{
+              backgroundGradientFrom: "#0000fff",
+              backgroundGradientTo: "#0000fff",
+              decimalPlaces: 0,
+              color:  () => `#323232`,
+              labelColor: () => `#323232`,
+              style: {
+                borderRadius: 0
+              },
+              propsForDots: {
+                r: "6",
+                strokeWidth: "2",
+                stroke: "#323232"
+              }
+            }}
+            bezier
+            style={{
+              marginVertical: 15,
+              borderRadius: 16,
+              left: -10
+            }}
+          />
+          </Chart>
+        </ContainerDashboard>
+        <Tabs></Tabs>
+      </Container>
     )
-}
+  }
 
-const styles = StyleSheet.create({
-    image: {
-        width: 60,
-        height: 100,
-        marginBottom: 50
-    }
-});
+
+}
