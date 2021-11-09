e992041bfb3854fd9a1aeb126c25905719cdb52c
Ycaro de Souza Lehmkuhl <65562799+ycarolehmkuhl@users.noreply.github.com>
2021-11-09 19:41:54 -0300

    new

--- src/screens/ServicosEditAdmin/index.js
+++ src/screens/ServicosEditAdmin/index.js
@@ -1,46 +1,49 @@
-import React, { useState, useContext } from 'react';
-import { useNavigation } from '@react-navigation/core';
-import { StyleSheet, ImageBackground, StatusBar, KeyboardAvoidingView, Platform } from 'react-native';
-import { TimePicker } from 'react-native-simple-time-picker';
-import { ServicesContext } from '../../config/ServicesProvider';
-import { 
-    Container, 
-    InputArea, 
-    CustomButton, 
-    CustomButtonText,
-    TitleTime
-} from './styles';
+import React, { useState, useContext } from "react";
+import { useNavigation } from "@react-navigation/core";
+import {
+  StyleSheet,
+  ImageBackground,
+  StatusBar,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import { TimePicker } from "react-native-simple-time-picker";
+import { ServicesContext } from "../../config/ServicesProvider";
+import {
+  Container,
+  InputArea,
+  CustomButton,
+  CustomButtonText,
+  TitleTime,
+} from "./styles";
 
-import SignInput from '../../components/SignInput';
+import SignInput from "../../components/SignInput";
 
-export default props => {
-
+export default (props) => {
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
 
-  const [serviceName, setServiceName] = useState('');
-  const [serviceValue, setServiceValue] = useState('');
+  const [serviceName, setServiceName] = useState("");
+  const [serviceValue, setServiceValue] = useState("");
 
   const { createService } = useContext(ServicesContext);
 
   return (
-
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={styles.container}>
-      <StatusBar
-        animated={true}
-        barStyle='dark-content' />
+      style={styles.container}
+    >
+      <StatusBar animated={true} barStyle="dark-content" />
       <InputArea>
         <SignInput
-          placeholder='Nome do serviço'
+          placeholder="Nome do serviço"
           value={serviceName}
-          onChangeText={t => setServiceName(t)}
+          onChangeText={(t) => setServiceName(t)}
         />
         <TitleTime>Tempo do serviço:</TitleTime>
         <TimePicker
           value={{ hours, minutes }}
-          onChange={value => {
+          onChange={(value) => {
             setHours(value.hours);
             setMinutes(value.minutes);
           }}
@@ -48,23 +51,33 @@ export default props => {
           minutesUnit="min"
         />
         <SignInput
-          placeholder='Valor do serviço'
+          placeholder="Valor do serviço"
           value={serviceValue}
-          onChangeText={t => setServiceValue(t)}
+          onChangeText={(t) => setServiceValue(t)}
           numeric={true}
         />
-        <CustomButton onPress={async () => await createService(serviceName, hours, minutes, serviceValue, props.navigation)}>
+        <CustomButton
+          onPress={async () =>
+            await createService(
+              serviceName,
+              hours,
+              minutes,
+              serviceValue,
+              props.navigation
+            )
+          }
+        >
           <CustomButtonText>Cadastrar</CustomButtonText>
         </CustomButton>
       </InputArea>
     </KeyboardAvoidingView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
